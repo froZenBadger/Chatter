7bf67781ec3fb15febb7be2e8a4b9f7f7e3c1509
Chris Palmer <cpalmer608@gmail.com>
2017-04-23 20:17:25 -0500

    Implemented time service

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-	function MainCtrl($scope, Room) {
+	function MainCtrl($scope, Room, Time) {
         this.title = "Chatter!";
 
         $scope.roomArray = Room.all;
@@ -8,23 +8,11 @@
             var data = $scope.room;
             $scope.roomArray.$add(data);
         };
-        var objToday = new Date(),
-            weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
-            dayOfWeek = weekday[objToday.getDay()],
-            domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
-            dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
-            months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-            curMonth = months[objToday.getMonth()],
-            curYear = objToday.getFullYear(),
-            curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
-            curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
-            curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
-            curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
-        var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
-        $scope.today = today;
+        $scope.today = Time.today;
+        $scope.day = Time.day;
 	}
 	
 	angular
 		.module('chatter')
-		.controller('MainCtrl', ['$scope', 'Room', MainCtrl]);
+		.controller('MainCtrl', ['$scope', 'Room', 'Time', MainCtrl]);
 })();
